eacd5316b11b504e594adcb8a73848e52fc58de3
Sandeep Kumar <sandeepkumar270724@gmail.com>
2026-07-20 02:56:06 +0530

    Time: 5 ms (33.05%) | Memory: 37.3 MB (46.35%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator{
+    stack<TreeNode*>st;
+    bool reverse = true;
+public:
+    BSTIterator(TreeNode* node , bool isreverse){
+        reverse = isreverse;
+        push_all(node);
+    }
+    int next(){
+        if(st.empty()) return -1;
+        TreeNode* temp = st.top();
+        st.pop();
+        if(!reverse){
+            push_all(temp->right);
+        }else{
+            push_all(temp->left);
+        }
+        return temp->val;
+    }
+    bool hasnext(){
+        return !st.empty();
+    }
+private:
+    void push_all(TreeNode* node){
+        while(node){
+            st.push(node);
+            if(!reverse){
+                node = node->left;
+            }else{
+                node = node->right;
+            }
+        }
+    }
+};
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if(!root) return false;
+        BSTIterator l(root,false);
+        BSTIterator r(root,true);
+
+        int i = l.next();
+        int j = r.next();
+        while(i<j){
+            if(i+j==k) return true;
+            else if (i+j<k) i = l.next();
+            else j=r.next();
+        }
+        return false;
+    }
+};
