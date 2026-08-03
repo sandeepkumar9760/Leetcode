49d190614b24cb5ce66d8821c3143b856d6cf2a7
Sandeep Kumar <sandeepkumar270724@gmail.com>
2026-08-04 00:53:05 +0530

    Time: 58 ms (49.54%) | Memory: 77.1 MB (41.20%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    double distance(vector<int>& point) {
-        int a = pow(point[0], 2);
-        int b = pow(point[1], 2);
-        return sqrt(a + b);
-    }
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<double, vector<int>>, vector<pair<double, vector<int>>>,
-                       greater<pair<double, vector<int>>>>
-            pq;
+        priority_queue<pair<double,vector<int>>>pq;
         int size = points.size();
         for (int i = 0; i < size; i++) {
-            double dist = distance(points[i]);
-            pq.push({dist, points[i]});
+            double dist = (points[i][0]*points[i][0]) + (points[i][1]*points[i][1]);
+            if(pq.size()!=k){
+                pq.push({dist,points[i]});
+            }else{
+                if(pq.top().first>dist){
+                    pq.pop();
+                    pq.push({dist,points[i]});
+                }
+            }
         }
         vector<vector<int>> ans;
         while (!pq.empty()) {
             ans.push_back(pq.top().second);
             pq.pop();
-            if (ans.size() == k) {
-                return ans;
-            }
         }
         return ans;
     }
