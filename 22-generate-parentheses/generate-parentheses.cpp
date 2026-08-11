0e4370c9564039902dafb027c8748f336d7a2458
Sandeep Kumar <sandeepkumar270724@gmail.com>
2026-08-12 00:22:15 +0530

    Time: 1 ms (85.61%) | Memory: 13.1 MB (78.91%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void backtrack(vector<string>& result, string current, int open, int close, int n) {
-        // Base case
-        if (current.length() == 2 * n) {
-            result.push_back(current);
+    void solve(vector<string>&ans , string &temp , int open , int close , int n){
+        if(open==n && close==n){
+            ans.push_back(temp);
             return;
         }
-
-        // Add '(' if possible
-        if (open < n) {
-            backtrack(result, current + "(", open + 1, close, n);
+        if(open<n){
+            temp.push_back('(');
+            solve(ans,temp,open+1,close,n);
+            temp.pop_back();
         }
+        if(close<open){
+            temp.push_back(')');
+            solve(ans,temp,open,close+1,n);
+            temp.pop_back();
 
-        // Add ')' if valid
-        if (close < open) {
-            backtrack(result, current + ")", open, close + 1, n);
         }
     }
-
     vector<string> generateParenthesis(int n) {
-        vector<string> result;
-        backtrack(result, "", 0, 0, n);
-        return result;
+        vector<string>ans;
+        string temp;
+        solve(ans,temp,0,0,n);
+        return ans;
     }
 };
