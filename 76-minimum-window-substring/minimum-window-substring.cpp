287c28b56446c5716b893067aa5548ee6682e715
Sandeep Kumar <sandeepkumar270724@gmail.com>
2026-08-31 16:05:46 +0530

    Time: 147 ms (7.63%) | Memory: 12 MB (7.73%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
-    bool check(vector<int>have , vector<int>want){
-        for(int i=0; i<256; i++){
-            if(have[i]<want[i]){
+    bool is_possible(vector<int>&want , vector<int>&have){
+        for(int i=0;i<256;i++){
+            if(have[i]>want[i]){
                 return false;
             }
         }
         return true;
     }
     string minWindow(string s, string t) {
-        string res = "";
-        if(s.length()<t.size()) return res;
+        if(t.size()>s.size()) return "";
+        // if(t==s) return s;
         vector<int>have(256,0);
         vector<int>want(256,0);
-        for(int i=0; i<t.length(); i++){
-            want[t[i]]++;
+        for(int i=0;i<t.size();i++){
+            have[t[i]-'A']++;
         }
-        int low = 0;
-        int start = 0;
-        int res_len = INT_MAX;
-        for(int high=0; high<s.size(); high++){
-            have[s[high]]++;
-            while(check(have,want)){
-               int len = high - low + 1;
-               if(res_len>len){
-                res_len = len;
-                start = low;
-               }
-               have[s[low]]--;
-               low++;
+        int low = 0 , high = 0;
+        int ans_low = 0;
+        int ans_high = 0;
+        bool possible = false;
+        int final_length = INT_MAX;
+        while(high<s.size()){
+            want[s[high]-'A']++;
+            while(is_possible(want,have)){
+                possible = true;
+                int crr_length = high - low + 1;
+                if(crr_length<final_length){
+                    ans_low = low;
+                    ans_high = high;
+                    final_length = crr_length;
+                }
+                want[s[low]-'A']--;
+                low++;
             }
-
+            high++;
         }
-        if(res_len==INT_MAX){
-            return "";
+        if(possible){
+            return s.substr(ans_low,ans_high-ans_low+1);
         }else{
-            return s.substr(start,res_len);
+            return "";
         }
     }
 };
