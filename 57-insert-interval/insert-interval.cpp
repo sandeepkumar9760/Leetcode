0564077e4b3949f7477ca63669aa9bd071babba8
Sandeep Kumar <sandeepkumar270724@gmail.com>
2026-09-06 01:08:07 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (47.26%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        if(intervals.empty() && newInterval.size()!=0){
-            return {newInterval};
-        }
-        if(intervals.empty()) return {};
+        if(intervals.empty()) return {newInterval};
+        int i=0;
+        int size = intervals.size();
         vector<vector<int>>ans;
-        bool insert = false;
-        for(int i=0; i<intervals.size(); i++){
-            if(insert==false && intervals[i][0]>=newInterval[0]){
-                ans.push_back(newInterval);
-                ans.push_back(intervals[i]);
-                insert = true;
-            }else{
-                ans.push_back(intervals[i]);
-            }
+        while(i<size && intervals[i][1]<newInterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
         }
-        if(insert==false){
-            ans.push_back(newInterval);
-        }
-        vector<vector<int>>result;
-        int start1 = ans[0][0];
-        int end1 = ans[0][1];
-        for(int i=1; i<ans.size(); i++){
-            int start2 = ans[i][0];
-            int end2 = ans[i][1];
-            if(end1>=start2){
-                end1 = max(end1 , ans[i][1]);
-                continue;
-            }else{
-                result.push_back({start1,end1});
-                start1 = start2;
-                end1=end2;
-            }
-        }
-        result.push_back({start1,end1});
-        return result;
 
+        while(i<size && intervals[i][0]<=newInterval[1]){
+            newInterval[0] = min(newInterval[0],intervals[i][0]);
+            newInterval[1] = max(newInterval[1],intervals[i][1]);
+            i++;
+        }
+
+        ans.push_back({newInterval});
+        while(i<size){
+            ans.push_back({intervals[i]});
+            i++;
+        }
+        return ans;
     }
 };
