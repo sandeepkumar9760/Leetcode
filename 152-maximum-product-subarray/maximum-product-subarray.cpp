e523962c5a67f4828de2b8c63ae7b3e838cd2fa7
Sandeep Kumar <sandeepkumar270724@gmail.com>
2026-09-05 15:36:09 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (5.77%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -6,13 +6,10 @@ public:
         int suffix = 1;
         for(int i=0;i<nums.size();i++){
             if(prefix==0)prefix=1;
-            prefix *= nums[i];
-            ans = max(ans,prefix);
-        }
-        for(int i=nums.size()-1;i>=0;i--){
             if(suffix==0)suffix=1;
-            suffix *= nums[i];
-            ans = max(ans,suffix);
+            prefix *= nums[i];
+            suffix *= nums[nums.size()-i-1];
+            ans = max(ans,max(prefix,suffix));
         }
         return ans;
     }
