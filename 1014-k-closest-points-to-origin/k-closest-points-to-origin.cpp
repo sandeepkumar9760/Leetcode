cad5c1f62fae7d6b23f3a73039d48fff1d296417
Sandeep Kumar <sandeepkumar270724@gmail.com>
2026-09-10 23:21:27 +0530

    Time: 52 ms (51.31%) | Memory: 77 MB (39.19%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
+    struct comp {
+        bool operator()(const pair<vector<int>,int>&a , const pair<vector<int>,int>&b){
+            return a.second < b.second;
+        }
+    };
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<double,vector<int>>>pq;
-        int size = points.size();
-        for (int i = 0; i < size; i++) {
-            int dist = (points[i][0]*points[i][0]) + (points[i][1]*points[i][1]);
+        vector<vector<int>>ans;
+        priority_queue<pair<vector<int>,int>,vector<pair<vector<int>,int>>,comp>pq;
+        for(int i=0;i<points.size();i++){
+            int a = (points[i][0]*points[i][0]) + (points[i][1]*points[i][1]);
             if(pq.size()!=k){
-                pq.push({dist,points[i]});
+                pq.push({points[i],a});
             }else{
-                if(pq.top().first>dist){
+                if(pq.top().second>a){
                     pq.pop();
-                    pq.push({dist,points[i]});
+                    pq.push({points[i],a});
                 }
             }
         }
-        vector<vector<int>> ans;
-        while (!pq.empty()) {
-            ans.push_back(pq.top().second);
+        while(!pq.empty()){
+            ans.push_back(pq.top().first);
             pq.pop();
         }
         return ans;
